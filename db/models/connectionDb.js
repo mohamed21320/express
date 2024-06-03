f8c7c51d4ccdd493c86214e2b2b1ce80dc3fd601
mohamed21320 <mo.elsayed150@gmail.com>
2024-06-03 14:35:44 +0300

    upload

--- db/models/connectionDb.js
+++ db/models/connectionDb.js
@@ -1,10 +1,6 @@
 import  mysql  from "mysql2";
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'test5',
-  });
+const connection = mysql.createConnection("mysql://u0c3vil1dfmxw6iw:z3SI73unTqwev1svKKwA@bt5c0hvdfwy9sqtebwo5-mysql.services.clever-cloud.com:3306/bt5c0hvdfwy9sqtebwo5");
   connection.connect((err, res) => {
 
     if (err) {
